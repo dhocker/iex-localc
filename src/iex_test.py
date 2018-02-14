6e5c2933b90d387cb339544abf4a8ff6f503324c
Dave Hocker <AtHomeX10@gmail.com>
2018-02-14 14:41:35 -0600

    Create a test harness for testing IEX extension functions outside of LibreOffice.

--- src/iex_test.py
+++ src/iex_test.py
@@ -1,69 +1,33 @@
 #
-# The IEX API appears to be most useful for current, relatively real time data.
-# It is not very useful for obtaining data that is more than about 30 days old (particularly prices).
-# It can obtain historical dividend data for the last 5 years.
-# It can also be useful for obtaining relatively static company information.
-# See https://iextrading.com/developer/docs/
+# iex_test - test harness for exercising LO extension functions outside
+# of LO. LO does not provide any sort of debugging aids for Python based
+# extensions, so this is used to compensate.
+# Copyright © 2018  Dave Hocker (email: Qalydon17@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE.md file for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (the LICENSE.md file).  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Candidate LibreOffice function signatures
-# IEXStocksCompany(ticker, key)
-# IEXStocksDividends(ticker, dividendrange, key)
-# IEXStocksEarnings(ticker, index, key)
+from iex_app_logger import AppLogger
+from iex_price import get_price
+# import datetime
+# import time
+# import json
+# Logger init
 
-from url_helpers import exec_request, setup_cacerts
-from iex_lib import IEXStocks
-import datetime
-import time
-import json
+the_app_logger = AppLogger("iex-extension")
+logger = the_app_logger.getAppLogger()
 
-# print ("Company info")
-# j = IEXStocks.get_company("SO")
-# print (j["status_code"])
-# print(j["result"])
-
-# print("Delayed Quote")
-# j = IEXStocks.get_delayed_quote("aapl")
-# print (j["status_code"])
-# print(json.dumps(j["result"], indent=4))
-# tv = float(j["result"]["delayedPriceTime"])
-# # To local time
-# dt = IEXStocks.get_formatted_datetime(tv)
-# print ("Delayed price time:", dt)
-
-# print("Dividends")
-# j = IEXStocks.get_dividends("SO", "1y")
-# print (j["status_code"])
-# print(j["result"])
-
-# print("Financials")
-# j = IEXStocks.get_financials("SO")
-# print (j["status_code"])
-# print(j["result"])
-
-# print("Quote")
-# j = IEXStocks.get_quote("IBM")
-# print (j["status_code"])
-# print(j["result"])
-#
-# # Time is unix time in milliseconds
-# # See https://github.com/iexg/IEX-API/issues/93
-# tv = float(j["result"]["latestUpdate"])
-# # To local time
-# dt = IEXStocks.get_formatted_datetime(tv)
-# print (dt)
-
-# print("Previous")
-# j = IEXStocks.get_previous("IBM")
-# print (j["status_code"])
-# print(j["result"])
-
-# print("Key Stats")
-# j = IEXStocks.get_stats("aapl")
-# print (j["status_code"])
-# print(json.dumps(j["result"], indent=4))
-
-print("Earnings")
-j = IEXStocks.get_earnings("aapl")
-print (j["status_code"])
-print(json.dumps(j["result"], indent=4))
+print("Price")
+logger.info("Testing iex_price.get_price()")
+j = get_price("aapl")
+print ("Price for aapl:", j)
